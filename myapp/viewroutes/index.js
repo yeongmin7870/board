fb8841286b7b8ce795661e08ce3862451b3a80a3
IWannaGoMars <73753121+yeongmin7870@users.noreply.github.com>
2022-12-21 20:20:06 +0900

    로그인, 회원가입, 홈화면 ejs 구현완료

--- myapp/viewroutes/index.js
+++ myapp/viewroutes/index.js
@@ -4,5 +4,10 @@ const router = express.Router();
 router.get('/', (req,res) => {
     res.render('index');
 });
-
+router.get('/register', (req,res) => {
+    res.render('register');
+});
+router.get('/home', (req,res) => {
+    res.render('home');
+});
 module.exports = router;
