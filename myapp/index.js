0c5bab26233d23dbd85825b42fe19d18cc7356b2
IWannaGoMars <73753121+yeongmin7870@users.noreply.github.com>
2023-01-11 21:37:42 +0900

    채팅 기능 구현중

--- myapp/index.js
+++ myapp/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cors = require('cors'); // cors - 클라이언트에서 ajax로 요청 시 CORS(다중 서버 접속) 지원
+const port = process.env.PORT || 3000;
+
 let bodyparser = require('body-parser');
 let cookieParser = require('cookie-parser');
 let methodOverride = require('method-override');
@@ -9,7 +13,7 @@ const userRouter = require('./routes/userroute');
 const viewRouter = require('./routes/viewroute')
 const cookieRouter = require('./routes/cookieroute');
 const boardRouter = require('./routes/boardroute');
-const chatRouter = require('./routes/chatroute');
+const chatfnc = require('./chat/chat');
 
 app.use(cookieParser());
 app.use(bodyparser.urlencoded({ extended: false }));
@@ -17,17 +21,25 @@ app.use(bodyparser.json());
 app.use(express.json());
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
+
 app.use('/v1', userRouter);
 app.use('/v2', viewRouter);
 app.use('/v3', boardRouter);
-app.use('/v4', chatRouter);
+
+app.use(cors());
 
 app.use('/cookies', cookieRouter);
 
-
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.set('io',io) // 전역변수로 등록 
 
-app.listen(port, () => {
-    console.log(`http://localhost:${port}/v2`);
+app.get('/chat', (req, res) => {
+    res.render('chat');
+});
+
+chatfnc(app); // 채팅 함수 호출
+
+http.listen(port, () => {
+    console.log(`http://localhost:${port}/v2/`);
 });
