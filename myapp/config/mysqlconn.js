571d4d2c4c5a3f243ffa17576daa225024cdc318
GiantTree7 <73753121+yeongmin7870@users.noreply.github.com>
2023-02-03 23:54:14 +0900

    Delete myapp/config directory

--- myapp/config/mysqlconn.js
+++
@@ -1,5 +0,0 @@
-const mysql2 = require('mysql2');
-const mysqlOption = require('./mysql2options.json');
-let pool = mysql2.createPool(mysqlOption);
-
-module.exports = pool;
--- myapp/config/secretkey.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    secretKey: '8a4ed0afb94b7953d71ecd40ba706c62fed349df728e3af6b772b45a1e314db55f5e7f66d1c7fe0bdb118b98369e757ea06f5bf110675b2862e9f803eead6c22',
-    option: {
-        algorithm: 'HS256',
-        expiresIn: '30m',
-        issuer: 'issuer'
-    }
-}
